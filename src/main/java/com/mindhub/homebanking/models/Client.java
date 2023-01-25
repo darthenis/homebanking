c3d57f7db460db719cdd8c1364dcb4ee123f0017
Emiliano Acevedo <81443527+darthenis@users.noreply.github.com>
2023-01-25 16:20:43 -0300

    Initial commit

---
+++ src/main/java/com/mindhub/homebanking/models/Client.java
@@ -0,0 +1,68 @@
+package com.mindhub.homebanking.models;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
+    private long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    public Client() {}
+    public Client(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
---
+++ src/main/resources/static/manager.js
@@ -0,0 +1,135 @@
+const { createApp } = Vue;
+
+createApp({
+  data() {
+    return {
+      clientsData: null,
+      newClient: {
+        firstName: "",
+        lastName: "",
+        email: ""
+      },
+      errors: {
+        firstName: "",
+        lastName: "",
+        email: ""
+      },
+      editErrors:{
+        firstName: "",
+        lastName: "",
+        email: ""
+      },
+      selectedClient: null,
+      selectedErrors: {
+        firstName: "",
+        lastName: "",
+        email: ""
+      },
+    };
+  },
+  created() {
+    this.loadData();
+  },
+  methods: {
+    loadData() {
+        axios
+        .get("http://localhost:8080/clients")
+            .then((res) => {
+        this.clientsData = res.data;
+        })
+        .catch((err) => console.log(err));
+    },
+    resetNewClient() {
+      this.newClient = { firstName: "", lastName: "", email: ""};
+    },
+    createClient() {
+
+      this.validateInputs(true, this.errors, this.newClient);
+
+      if(!Object.values(this.errors).some(e => e !== "")){
+
+            axios
+            .post("http://localhost:8080/clients", this.newClient)
+            .then((res) => {
+              this.loadData();
+              this.resetNewClient();
+            })
+            .catch((err) => console.log(err));
+
+        }
+
+    
+    },
+    deleteClient() {
+      axios.delete(this.selectedClient._links.self.href).then((res) => {
+        
+        this.loadData();
+        this.selecteClient = null;
+      
+      });
+    },
+    selectClient(client) {
+      this.selectedClient = {...client};
+    },
+    cancelEdit() {
+      this.selectedClient = null;
+    },
+    putClient() {
+
+      this.validateInputs(true, this.editErrors, this.selectedClient);
+
+      if(!Object.values(this.editErrors).some(e => e !== "")){
+
+        axios.put(this.selectedClient._links.self.href, this.selectedClient).then(() => {
+
+          this.loadData();
+
+        });
+      }
+    },
+    validateInputs(isSending, errors, data){
+
+      for(let [key] of Object.entries(errors)){
+
+          if (isSending && !data[key]){ 
+            
+            errors[key] = "*required field";
+          
+          } else if(data[key]){ 
+            
+            errors[key] = ""; 
+          
+          }
+
+
+          if(data[key]){
+
+              if(!/^[A-Za-zÁÉÍÓÚáéíóúñÑ ]+$/g.test(data[key]) && 
+                  (   key === "firstName" || 
+                      key === "lastName")) errors[key] = "*just introduce letters";
+
+              else if(key === "email" && 
+                      !data[key].toLowerCase()
+                      .match(
+                        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+                      )) errors[key] = "*introduce an validate email";
+
+        }
+      }
+    }
+
+  },
+  computed: {
+    validateChange(){
+
+      this.validateInputs(false, this.errors, this.newClient);
+
+    },
+    validateChangesEdit(){
+
+      this.validateInputs(false, this.editErrors, this.selectedClient)
+
+    }
+
+}
+}).mount("#app");
